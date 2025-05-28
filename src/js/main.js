948326e5afcf8f8ce853be3704fafdaf65311ce9
Cecilia Gutierrez <ceciliagutierrezurbano@gmail.com>
2025-05-27 22:07:32 -0600

    "feat: importe header.js"

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
 import insertFooter from "../modules/footer/footer.js";
+import insertHeader from "../modules/header/header.js";
 
+insertHeader(document.getElementById("header"));
 insertFooter(document.getElementById("footer"));
+
